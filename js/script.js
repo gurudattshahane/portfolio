2e4823c1dbaf7f5b0afff17b7cd7fef557f82397
Gurudatt Shahane <gurudattshahane50@gmail.com>
2022-08-11 11:03:24 +0530

    Hamburger and Font fixes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll('.navbar ul li');
+const checkbox = document.querySelector("#hamburger");
+console.log(links);
+links.forEach(link => {
+    link.addEventListener('click', ()=>{
+        links.forEach(li =>{ li.classList.remove("active");});
+        link.classList.add("active");
+        checkbox.checked = false;
+    })
+});
