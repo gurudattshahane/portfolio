5736fe53ff53f9619b27f0e3f5edf2047e1b9b24
Gurudatt Shahane <gurudattshahane50@gmail.com>
2022-08-11 19:44:22 +0530

    Header position made fixed, some font-size changes in skills section, Added header bg change with scroll in JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* Changing background of current navbar section */
 const links = document.querySelectorAll('.navbar ul li');
 const checkbox = document.querySelector("#hamburger");
 
@@ -8,6 +9,30 @@ links.forEach(link => {
         checkbox.checked = false;
     })
 });
+
+let myNav = document.querySelector('header');
+window.onscroll = function () { 
+    "use strict";
+    if (window.scrollY >= myNav.scrollHeight ) {
+        myNav.classList.add("navbar-bg-colored");
+        myNav.classList.remove("navbar-bg-transparent");
+    } 
+    else {
+        myNav.classList.add("navbar-bg-transparent");
+        myNav.classList.remove("navbar-bg-colored");
+    }
+    
+    // Changing background of navbar list based on position
+    console.log(window.scrollY);
+    if(window.scrollY <= (window.innerHeight/2)){
+        links.forEach(li =>{ li.classList.remove("active");});
+        links[0].classList.add('active');
+    }else{
+        links.forEach(li =>{ li.classList.remove("active");});
+        links[3].classList.add('active');
+    }
+};
+
 
 const skills_data = document.querySelectorAll('.skills-data');
 const skills_progress_tags = document.querySelectorAll('.skills-progress');
